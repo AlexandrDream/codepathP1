5931c937655f8f332082b1705e287d145b733642
AlexandrDream <98363672+AlexandrDream@users.noreply.github.com>
2023-02-07 17:08:31 -0800

    Add files via upload

---
+++ Project 1/Project 1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project 1
+//
+//  Created by user233227 on 2/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func BackstoryButton(_ sender: Any) {
+    }
+    @IBAction func WeaknessButton(_ sender: Any) {
+    }
+    @IBAction func LiftButton(_ sender: Any) {
+    }
+    
+    
+}
+
